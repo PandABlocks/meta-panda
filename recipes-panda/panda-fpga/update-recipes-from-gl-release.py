cf66a959a6a85e871752de7999e0fba3e6f2b1cd
Emilio Perez Juarez <emiliopeju@gmail.com>
2026-05-22 12:12:38 +0100

    Make update script consider slowFPGA too

--- recipes-panda/panda-fpga/update-recipes-from-gl-release.py
+++ recipes-panda/panda-fpga/update-recipes-from-gl-release.py
@@ -6,7 +6,11 @@ import os
 import re
 import requests
 
+from pathlib import Path
+
 GITLAB_API_URL="https://gitlab.diamond.ac.uk/api/v4"
+PANDABLOCKS_FPGA_PROJECT_ID = 7925
+PANDABLOCKS_SLOWFPGA_PROJECT_ID = 7758
 
 
 def parse_args():
@@ -15,9 +19,10 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_links(tag):
+def get_links(tag, project_id=PANDABLOCKS_FPGA_PROJECT_ID):
     return requests.get(
-        f'{GITLAB_API_URL}/projects/7925/releases/{tag}/assets/links').json()
+        f'{GITLAB_API_URL}/projects/{project_id}/releases/{tag}'
+        '/assets/links').json()
 
 
 def remove_old_recipes(pkg_name):
@@ -41,10 +46,14 @@ def handle_fpga_boot(link_info):
     url = link_info['direct_asset_url']
     print(f'FPGA boot asset URL: {url}')
     INCLUDE_FILE = 'panda-fpga-boot-uris.inc'
-    sha256 = get_sha256_from_url(url)
     with open(INCLUDE_FILE, 'r') as f:
         content = f.read()
 
+    if f'SRC_URI:{machine} = "{url};name={machine}-boot"' in content:
+        print(f'Skipping {name} (recipe already up to date)')
+        return
+
+    sha256 = get_sha256_from_url(url)
     content = re.sub(
         rf'SRC_URI:{machine} = ".*"',
         f'SRC_URI:{machine} = "{url};name={machine}-boot"',
@@ -62,6 +71,10 @@ def handle_fpga(link_info):
     # Remove _all.ipk ending
     name = link_info['name'][:-8]
     url = link_info['direct_asset_url']
+    if Path(f'{name}.bb').exists():
+        print(f'Skipping {name} (recipe already exists)')
+        return
+
     print(f'FPGA asset URL: {url}')
     remove_old_recipes(name.split('_')[0])
     sha256 = get_sha256_from_url(url)
@@ -76,7 +89,8 @@ SRC_URI[sha256sum] = "{sha256}"
 
 def main():
     args = parse_args()
-    for link_info in get_links(args.tag):
+    for link_info in get_links(args.tag, PANDABLOCKS_FPGA_PROJECT_ID) + \
+                        get_links(args.tag, PANDABLOCKS_SLOWFPGA_PROJECT_ID):
         name = link_info['name']
         if not name.endswith('.ipk'):
             print(f'Skipping {name} (not an ipk)')
@@ -85,7 +99,7 @@ def main():
         print(f'Handling {name}')
         if name.startswith('panda-fpga-boot'):
             handle_fpga_boot(link_info)
-        elif name.startswith('panda-fpga'):
+        elif name.startswith('panda-fpga') or name.startswith('panda-slowfpga'):
             handle_fpga(link_info)
 
 
